0175b0c03dce89ccc8b546ba05724e92e394ef92
Nitish kumar <kumarnitish754@gmail.com>
2017-12-12 23:41:59 +0530

    add files

---
+++ content.js
@@ -0,0 +1,11 @@
+alert("Hello from your chrome extension")
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
